8550d4b593793a29348da149d3d5606cd4e3e23b
Utwo <mihai.legat@gmail.com>
2018-07-25 18:59:32 +0300

    Resolve all tslint errors.

--- src/generators/project.ts
+++ src/generators/project.ts
@@ -14,44 +14,31 @@ export default class NuxtProjectGenerator extends ProjectGenerator {
   }
 
   public generate(project: any, options: any = {}): FileSet {
-    const { name, components, pages } = project
+    const { components, pages } = project
 
     const result = new FileSet()
-    result.addFile(
-      'package.json',
-      packageRenderer(project)
-    )
+    result.addFile('package.json', packageRenderer(project))
 
     if (components) {
-      Object.keys(components).map(componentName => {
+      Object.keys(components).map((componentName) => {
         const component = components[componentName]
         const componentResults = this.componentGenerator.generate(component)
-        componentResults.getFileNames().map(fileName => {
-          result.addFile(
-            `components/${fileName}`,
-            componentResults.getContent(fileName)
-          )
+        componentResults.getFileNames().map((fileName) => {
+          result.addFile(`components/${fileName}`, componentResults.getContent(fileName))
         })
       })
     }
 
     if (pages) {
-      Object.keys(pages).map(pageName => {
+      Object.keys(pages).map((pageName) => {
         const page = pages[pageName]
         const pageResults = this.componentGenerator.generate(page)
-        pageResults.getFileNames().map(fileName => {
-          result.addFile(
-            `pages/${fileName}`,
-            pageResults.getContent(fileName)
-          )
+        pageResults.getFileNames().map((fileName) => {
+          result.addFile(`pages/${fileName}`, pageResults.getContent(fileName))
         })
       })
     }
 
     return result
-  }
-
-  public publish(path: string, archive: boolean = false): void {
-
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Generator, FileSet } from '@teleporthq/teleport-lib-js'
-
 import NuxtComponentGenerator from './generators/component'
 import NuxtProjectGenerator from './generators/project'
 
 export default class TeleportGeneratorNuxt extends Generator {
+  // @todo: can we avoid redeclaring componentGenerator and projectGenerator since they exist on Generator?
+  public componentGenerator: NuxtComponentGenerator
+  public projectGenerator: NuxtProjectGenerator
+
   constructor() {
     super('nuxt-generator', 'nuxt')
 
@@ -11,7 +14,7 @@ export default class TeleportGeneratorNuxt extends Generator {
     this.projectGenerator = new NuxtProjectGenerator(this, this.componentGenerator)
   }
 
-  public generateComponent(component: any, options: any): FileSet {
+  public generateComponent<T, U>(component: T, options: U): FileSet {
     return this.componentGenerator.generate(component, options)
   }
 
